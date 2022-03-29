bdc95fd77cc632a0c11bf9096733bfe53ffa2d20
XiNGRZ <hi@xingrz.me>
2022-03-29 16:53:02 +0800

    Revert "Install VC++ 10.0 redist"
    This reverts commit 3c8651faa8ef77f888f4ea8a79f9cd55c617c8ba.

--- src/install.ts
+++
@@ -1,6 +0,0 @@
-import { execFileSync } from 'child_process';
-import { resolve } from 'path';
-
-if (process.platform == 'win32') {
-  execFileSync(resolve(__dirname, '..', 'usb2xxx', 'win32', 'x64', 'vcredist_x64.exe'), ['/q']);
-}
