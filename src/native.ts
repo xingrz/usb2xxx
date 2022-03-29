b757b2f9cb90ee7129ec3bc668c726c7c7a96e80
XiNGRZ <hi@xingrz.me>
2022-03-29 16:56:22 +0800

    Revert "Explicitly load libusb"
    This reverts commit ba1419af5237b81eb27cbc7abcdc63a132c1e6ad.

--- src/native.ts
+++ src/native.ts
@@ -1,11 +1,9 @@
-import { join, resolve } from 'path';
+import { resolve } from 'path';
 import {
-  DynamicLibrary,
   LibraryObject,
   LibraryObjectDefinitionBase,
   LibraryObjectDefinitionInferenceMarker,
   LibraryObjectDefinitionToLibraryDefinition,
-  RTLD_NOW,
 } from '@xingrz/ffi-napi';
 
 export const LIB_FILE = (() => {
@@ -16,22 +14,11 @@ export const LIB_FILE = (() => {
   }
 })();
 
-const LIBUSB_FILE = (() => {
-  switch (process.platform) {
-    case 'darwin': return 'libusb-1.0.0.dylib';
-    case 'linux': return 'libusb-1.0.so';
-    case 'win32': return 'libusb-1.0.dll';
-  }
-})();
-
-if (!LIB_FILE || !LIBUSB_FILE) {
+if (!LIB_FILE) {
   throw new Error(`USB2XXX is not support for ${process.platform}/${process.arch}`);
 }
 
-const LIB_ROOT = resolve(__dirname, '..', 'usb2xxx', process.platform, process.arch);
-new DynamicLibrary(join(LIB_ROOT, LIBUSB_FILE), RTLD_NOW);
-
-export const LIB_PATH = join(LIB_ROOT, LIB_FILE);
+export const LIB_PATH = resolve(__dirname, '..', 'usb2xxx', process.platform, process.arch, LIB_FILE);
 
 type LibraryObjectDefinition = LibraryObjectDefinitionBase | LibraryObjectDefinitionInferenceMarker;
 
